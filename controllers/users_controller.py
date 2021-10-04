fdf7c2bfce837f291ba88c4be92d40e64001bd79
Coralys M Cortes Soto <coralys.cortes1@upr.edu>
2021-10-04 09:00:42 -0400

    Created user authentication

--- controllers/users_controller.py
+++ controllers/users_controller.py
@@ -5,6 +5,9 @@ from models.users_dao import UserDao
 
 class UserController:
 
+    def __init__(self):
+        self.dao = UserDao()
+
     def build_attr_dict(self, data):
         return {
             'user_id': data[0],
@@ -15,7 +18,18 @@ class UserController:
         }
 
     def create_user(self, user_info):
-        dao = UserDao()
-        user = dao.create_user(user_info)
+        user = self.dao.create_user(user_info)
         return jsonify(self.build_attr_dict(user)), 201
+
+    def login_user(self, credentials):
+        user = self.dao.login_user(credentials)
+        if user:
+            dict = {
+                'user_id': user[0],
+                'type': user[1],
+            }
+            return jsonify(dict), 200
+        else:
+            return jsonify("Invalid credentials"), 401
+
 
