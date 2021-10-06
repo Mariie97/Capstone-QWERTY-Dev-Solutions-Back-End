0ae56740226a08f9793e4ad84ef728b3024501dd
noelvalentin <noel.valentin2@upr.edu>
2021-10-06 10:53:38 -0400

    Adjusted the edit_user method in Users DAO.

--- models/users_dao.py
+++ models/users_dao.py
@@ -28,11 +28,15 @@ class UserDao(MainDao):
         user_info = cursor.fetchone()
         self.conn.commit()
 
-        if user_info[6] == 'null':
-            query1 = 'insert into address (street, city, zipcode) values (%s, %s, %s);'
+        if user_info[6] is None:
+            query1 = 'insert into address (street, city, zipcode) values (%s, %s, %s) returning address_id;'
             cursor.execute(query1, (data['street'], data['city'], data['zipcode']))
+
+            address_info = cursor.fetchone()
+            self.conn.commit()
+
             query2 = 'update users set address_id = %s where user_id = %s;'
-            cursor.execute(query2, (user_info[6], data['user_id']))
+            cursor.execute(query2, (address_info[0], data['user_id']))
 
         else:
             query = 'update address set street = %s, city = %s, zipcode = %s where address_id = %s;'
