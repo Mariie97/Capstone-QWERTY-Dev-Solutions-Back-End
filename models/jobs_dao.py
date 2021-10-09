cc595a54d647dcf5d96eaaa855eb8e5fc98e64c5
Coralys M Cortes Soto <coralys.cortes1@upr.edu>
2021-10-08 21:46:48 -0400

    Created jobs dao and the model to retrieve all requests from a job

---
+++ models/jobs_dao.py
@@ -0,0 +1,18 @@
+from models.main_dao import MainDao
+
+
+class JobDao(MainDao):
+
+    def get_requests_list(self, data):
+        cursor = self.conn.cursor()
+        query = 'select user_id, first_name, last_name, image, date' \
+                'from requests as R inner join users as U on R.student_id=U.user_id' \
+                'where job_id=%s' \
+                'order by date asc;'
+
+        cursor.execute(query, (data['job_id'], ))
+        requests_list = []
+        for row in cursor:
+            requests_list.append(row)
+        return requests_list
+
