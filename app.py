ce06d02da4d2ece06c55915ad38350188d033a0f
Coralys M Cortes Soto <coralys.cortes1@upr.edu>
2021-10-14 10:00:39 -0400

    Merged master and clean imports

--- app.py
+++ app.py
@@ -9,11 +9,8 @@ from config.config import JWT_SECRET_KEY, JWT_TOKEN_LOCATION, JWT_ACCESS_TOKEN_E
     SECRET_KEY
 from controllers.jobs_controller import JobController
 from controllers.users_controller import UserController
-from utilities import SUPERUSER_ACCOUNT, CLIENT_ACCOUNT, STUDENT_ACCOUNT, validate_assign_job_data, \
-    validate_password_info, validate_email, validate_user_info, validate_login_data, STATUS_CODE, upload_image_aws, \
-from utilities import validate_user_info, validate_login_data, STATUS_CODE, SUPERUSER_ACCOUNT, CLIENT_ACCOUNT, \
-    STUDENT_ACCOUNT, validate_email, validate_password_info
-from utilities import validate_user_info, validate_login_data, STATUS_CODE, upload_image_aws, generate_profile_pic_url, \
+from utilities import SUPERUSER_ACCOUNT, CLIENT_ACCOUNT, STUDENT_ACCOUNT, validate_email, validate_password_info, \
+    validate_assign_job_data, validate_user_info, validate_login_data, STATUS_CODE, upload_image_aws, \
     validate_profile_data
 
 app = Flask(__name__)
